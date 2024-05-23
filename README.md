103e9cebfc37092610608628ce7ff1f217c968fe
99gautham <99gautham@gmail.com>
2024-05-23 15:20:34 +0700

    code add

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Node_Cluster
+
+This is a simple application to demonstrate how clustering works in Node.js using the cluster module and Express.
+
+How it works
+The application uses the cluster module to fork multiple worker processes, equal to the number of CPU cores available on the machine. Each worker process runs an Express server, allowing the application to handle more concurrent requests by utilizing multiple CPU cores.
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const cluster = require("node:cluster");
+const os = require("os");
+
+const totalCPUs = os.cpus().length;
+
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  const app = express();
+  const PORT = 8000;
+
+  app.get("/", (req, res) => {
+    return res.json({ message: `Hello from Express Server ${process.pid}` });
+  });
+
+  app.listen(PORT, () => console.log(`Server Started At PORT:${PORT}`));
+}
